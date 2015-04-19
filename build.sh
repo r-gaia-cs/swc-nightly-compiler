96ae7ee0fbf9d47461487824092b1285cf1a0879
Raniere Silva <raniere@ime.unicamp.br>
2015-04-19 00:06:01 -0300

    Add checkout to allow pull

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ source lessons.sh
 for lesson in $(find ${NIGHTLY_DIR} -mindepth 1 -maxdepth 1 -type d)
 do
     cd ${lesson}
+    git checkout -f gh-pages
     git pull origin gh-pages
     make -B preview
     cd -
